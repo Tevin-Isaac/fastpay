6dd5e1af7785587da7a8452faf976a45460155fe
cosm0x <120276803+cosm0x@users.noreply.github.com>
2024-05-07 23:52:45 +0100

    init: wagmi and prisma

---
+++ src/providers/index.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import * as React from "react";
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  getDefaultConfig,
+} from "@rainbow-me/rainbowkit";
+import { coinbaseWallet } from "@rainbow-me/rainbowkit/wallets";
+import { baseSepolia } from "wagmi/chains";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+
+const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID as string;
+
+const { wallets } = getDefaultWallets();
+
+const config = getDefaultConfig({
+  appName: "FastPay",
+  projectId,
+  wallets: [
+    ...wallets,
+    {
+      groupName: "Other",
+      wallets: [coinbaseWallet],
+    },
+  ],
+  chains: [baseSepolia],
+  ssr: true,
+});
+
+const queryClient = new QueryClient();
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
